8027968eb451342b28e166e281ebdd6fe4457942
William Inyam <sw.inyam@outlook.com>
2024-03-28 00:17:59 +0000

    style(b_trees:15): bettyfy file

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,6 +16,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	return ( binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right) );
+	return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
 }
