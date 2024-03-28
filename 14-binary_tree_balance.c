6d3a1cd25a2786879cead09ba51742cfa5cc2dfc
William Inyam <sw.inyam@outlook.com>
2024-03-28 01:23:15 +0000

    ci(b_trees:14): change dependencies location

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,20 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - a function that measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the binary tree
+ *
+ * Return: height if @tree is NOT NULL
+ *         0 otherwise
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	return (binary_tree_height_int(tree));
+}
+
+
+/**
  * _int_max - a function that returns the maximum between integers
  *
  * It's variadic and takes in as many ints as desired
