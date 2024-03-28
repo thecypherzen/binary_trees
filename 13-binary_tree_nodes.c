ec3bfab85238c2585ba30ac1b70021546a9942a0
William Inyam <sw.inyam@outlook.com>
2024-03-28 00:11:22 +0000

    style(b_trees:13): bettyfy file

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,6 +15,6 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	if (!tree || (!tree->left && !tree->right))
 		return (0);
-	return ( 1 + binary_tree_nodes(tree->left) +
+	return (1 + binary_tree_nodes(tree->left) +
 			binary_tree_nodes(tree->right));
 }
