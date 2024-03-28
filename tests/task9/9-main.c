38a1330a90c7aa7e3a68fcce0011acd407ebeef4
William Inyam <sw.inyam@outlook.com>
2024-03-28 08:59:43 +0000

    fix(b_trees:9): fix null tree error

--- tests/task9/9-main.c
+++ tests/task9/9-main.c
@@ -13,6 +13,8 @@ int main(void)
     size_t height;
 
     root = binary_tree_node(NULL, 98);
+    height = binary_tree_height(root);
+    printf("Height from %d: %lu\n", root->n, height);
     root->left = binary_tree_node(root, 12);
     root->right = binary_tree_node(root, 402);
     binary_tree_insert_right(root->left, 54);
@@ -25,5 +27,7 @@ int main(void)
     printf("Height from %d: %lu\n", root->right->n, height);
     height = binary_tree_height(root->left->right);
     printf("Height from %d: %lu\n", root->left->right->n, height);
+    height = binary_tree_height(NULL);
+    printf("Height of NULL %lu\n", height);
     return (0);
 }
