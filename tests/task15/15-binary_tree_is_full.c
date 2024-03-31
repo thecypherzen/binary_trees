eecfd59052adead55b4d3eae64419220ce3b6d2c
William Inyam <sw.inyam@outlook.com>
2024-03-31 10:31:13 +0000

    docs(b_trees:15): remove needless headers

--- tests/task15/15-binary_tree_is_full.c
+++ tests/task15/15-binary_tree_is_full.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_tree_is_full - a function that checks if a binary tree is full
- *
+ * A full binary tree has each node with at least 2 children.
  * @tree: pointer to the root node of the tree to check
  *
  * Return: 0 if @tree is NULL
@@ -16,6 +16,6 @@ int binary_tree_is_full(const binary_tree_t *tree)
 		return (0);
 	if (!tree->left && !tree->right)
 		return (1);
-	return ( binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right) );
+	return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
 }
