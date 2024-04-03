258433d443a476481a4824e55fdf85e2f118a05f
William Inyam <sw.inyam@outlook.com>
2024-04-03 20:10:18 +0000

    build(b_trees:26(112)): add task solution and test

---
+++ tests/task26/112-array_to_bst.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_bst - builds a bst from an array
+ *
+ * @array: pointer to array
+ * @size: size of array
+ *
+ * If value is aready in tree, it's ignored
+ *
+ * Return: a pointer to bst root node
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+
+	bst_t *root = NULL;
+	size_t i;
+
+	if (!size || !array)
+		return (NULL);
+
+	for (i = 0;i < size; i++)
+	{
+		bst_insert(&root, array[i]);
+	}
+	return (root);
+}
